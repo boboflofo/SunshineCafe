46f0b670d55b848788bb783213e49e93accd7fe1
Jonathan Song <jonats10@uci.edu>
2024-01-26 13:29:10 -0800

    add order model logic

--- SunshineCafe/Models/Vendor.cs
+++ SunshineCafe/Models/Vendor.cs
@@ -5,6 +5,8 @@ namespace SunshineCafe.Models
   public class Vendor
   {
     public string Name { get; set; }
+
+    public string Description { get; set; }
     public int Id { get; }
     // public List<Order> Orders { get; set; }
     private static List<Vendor> _instances = new List<Vendor> {};
